e3db414c4f31c5c3a91eeb5410f58b105c0405dd
AmjadChowdhury <amjadchowdhury247@gmail.com>
2024-04-04 16:17:01 +0100

    alignment

--- src/components/FeaturedList/FeaturedList.jsx
+++ src/components/FeaturedList/FeaturedList.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import Job from "../Job/Job";
 
 
 const FeaturedList = () => {
@@ -12,6 +13,11 @@ const FeaturedList = () => {
         <div>
             <h1 className="text-2xl text-center">This is featured List</h1>
             <h1 >Jobs length : {jobs.length}</h1>
+            <div>
+                {
+                    jobs.map(job => <Job key={job.id} job={job}></Job>)
+                }
+            </div>
         </div>
     );
 };
---
+++ src/components/Job/Job.jsx
@@ -0,0 +1,12 @@
+
+
+const Job = ({job}) => {
+    const {id,logo,job_title,company_name,remote_or_onsite,location,job_type,salary} =job
+    return (
+        <div>
+            <h1>Job</h1>
+        </div>
+    );
+};
+
+export default Job;
