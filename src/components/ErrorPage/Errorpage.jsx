b2fe752563e17583538d497cf2b218405c3a1047
AmjadChowdhury <amjadchowdhury247@gmail.com>
2024-10-12 17:33:46 +0100

    handle error page and some edit

--- src/components/ErrorPage/Errorpage.jsx
+++ src/components/ErrorPage/Errorpage.jsx
@@ -1,11 +1,13 @@
-import { Link } from "react-router-dom";
+import { Link, useRouteError } from "react-router-dom";
 
 
 const Errorpage = () => {
+    const error = useRouteError()
     return (
-        <div>
-            <h1 className="text-3xl font-bold">Oops!!!</h1>
-            <button className="btn btn-primary"><Link to="/">Go Back</Link></button>
+        <div className="text-center mt-5">
+            <p className="text-6xl font-extrabold text-red-700">{error.status}</p>
+            <h1 className="text-3xl font-bold text-red-900">{error.statusText}</h1>
+            <Link to="/"><button className="btn bg-red-950 text-white font-bold">Go Back</button></Link>
         </div>
     );
 };
